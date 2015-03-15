4df3465fc4027673ebfbbec91afdcc2a880846fc
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-03-15 18:46:15 -0500

    drafting js equation to work with new conditions API from wunderground

--- homework/Zoe_HW/API/js/main.js
+++ homework/Zoe_HW/API/js/main.js
@@ -1,45 +1,17 @@
-var solution;
-
-var degrees = document.getElementById('number').value;
-
-var optionChosen = document.getElementsByName('temptype');
-
-function determineConverter(e) {
- 
+"use strict";
+function getWeather() {
+ $.ajax({
+       url: "http://api.wunderground.com/api/5d0612849c444de1/conditions/q/37209.json",
+     dataType: "jsonp",
+     success : function(data){
+       console.log(data);
+       var time = console.log(data.current_observation.observation_time_rfc822);
+       var degrees = console.log(data.current_observation.temp_f);
+       var location = console.log("add zip");
+     }
+ });
 }
 
-
-var button = document.getElementById("displaysconvert");
-
-button.onclick = determineConverter;
-
-'use strict';
-
-//What is the phase of the moon tonight?
-
-// function moonPhase() {
-//  $.ajax({
-//        url: "http://api.wunderground.com/api/5d0612849c444de1/astronomy/q/37210.json",
-//      dataType: "jsonp",
-//      success : function(data){
-//        console.log(data);
-//        console.log(data.moon_phase);
-//        console.log(data.moon_phase.phaseofMoon)
-//      }
-//  });
-// }
-// moonPhase();
-// 
-
-//In what year was the record low temperature for today's date?  use almanac
-// function recordLow() {
-//  $.ajax({
-//    url: "http://api.wunderground.com/api/5d0612849c444de1/almanac/q/37210.json",
-//    dataType: "jsonp",
-//    success : function(data){
-//      console.log(data);
-//      console.log(data.almanac.temp_low.recordyear)
-//    }
-//  });
-// }
-// recordLow();
+$( document ).ready(function() {
+    console.log(getWeather());
+});
