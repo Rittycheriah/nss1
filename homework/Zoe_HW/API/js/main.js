3ec22176041406d2a440e400fcbda93ce494f960
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-03-15 22:25:04 -0500

    fully styled

--- homework/Zoe_HW/API/js/main.js
+++ homework/Zoe_HW/API/js/main.js
@@ -1,45 +1,38 @@
 "use strict";
 
-var results = document.getElementById("resultdiv");
-
+//function for getting API results
 function getWeather() {
- var resultsReturn = []
- 
- $("button#daButton").click(function () {
-   var zipCode = $("textarea#zip").val();
- })
 
- $.ajax({
-     url: "http://api.wunderground.com/api/5d0612849c444de1/conditions/" + zipCode + ".json",
-     dataType: "jsonp",
-     success : function(data){
-       console.log(data);
-       //Return results from API 
-       var time = console.log(data.current_observation.observation_time_rfc822);
-       var degrees = console.log(data.current_observation.temp_f);
-       var location = console.log("add zip");
-       resultsReturn = time, degrees, location;
-       results.innerHTML = resultsReturn;
-       console.log(resultsReturn)
-     }
+//results returning as an array
+  var resultsReturn = []
+
+ //getting the input from textarea
+  // $("button#daButton").click(function () {
+  //     var zipCode = "37209";
+  //     //$("textarea#zip").val('37209');
+  //  })
+
+//API gets from zipCode
+   $.ajax({
+       url: "http://api.wunderground.com/api/5d0612849c444de1/conditions/" + zipCode +".json",
+       dataType: "jsonp",
+       success : function(data){
+         console.log(data);
+         //Return results from API 
+         var time = console.log(data.current_observation.observation_time_rfc822);
+         var degrees = console.log(data.current_observation.temp_f);
+         var location = console.log("add zip");
+         resultsReturn = time, degrees, location;
+         results.innerHTML = resultsReturn;
+         console.log(resultsReturn)
+       } 
  });
+
+//writing to div
+document.getElementById('resultdiv').innerHTML= (time, degrees, location);
+
 }
 
-// $("button#daButton").click(function () {
-//   var zipode = $("textarea#zip").val();
-// }
-//Create method of getting input information from HTML
-// $("textarea#zip").onclick(function() {
-//     var zipCode = $("#zip").val('').toString('');
-//     console.log(zipCode);
-//   })
-
-//Use (e)*click to trigger function
-// getWeather(e);
-
-//Call wunderground infromation using user input zip
-
-
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log(getWeather());
 });
