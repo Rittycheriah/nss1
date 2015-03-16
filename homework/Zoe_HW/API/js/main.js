ac37ec5aacf166a6d94dccd60d5787f700fa6d18
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-03-15 21:28:19 -0500

    set up button to be received by URL

--- homework/Zoe_HW/API/js/main.js
+++ homework/Zoe_HW/API/js/main.js
@@ -1,16 +1,44 @@
 "use strict";
+
+var results = document.getElementById("resultdiv");
+
 function getWeather() {
+ var resultsReturn = []
+ 
+ $("button#daButton").click(function () {
+   var zipCode = $("textarea#zip").val();
+ })
+
  $.ajax({
-       url: "http://api.wunderground.com/api/5d0612849c444de1/conditions/q/37209.json",
+     url: "http://api.wunderground.com/api/5d0612849c444de1/conditions/" + zipCode + ".json",
      dataType: "jsonp",
      success : function(data){
        console.log(data);
+       //Return results from API 
        var time = console.log(data.current_observation.observation_time_rfc822);
        var degrees = console.log(data.current_observation.temp_f);
        var location = console.log("add zip");
+       resultsReturn = time, degrees, location;
+       results.innerHTML = resultsReturn;
+       console.log(resultsReturn)
      }
  });
 }
+
+// $("button#daButton").click(function () {
+//   var zipode = $("textarea#zip").val();
+// }
+//Create method of getting input information from HTML
+// $("textarea#zip").onclick(function() {
+//     var zipCode = $("#zip").val('').toString('');
+//     console.log(zipCode);
+//   })
+
+//Use (e)*click to trigger function
+// getWeather(e);
+
+//Call wunderground infromation using user input zip
+
 
 $( document ).ready(function() {
     console.log(getWeather());
