c17052d675b093bd6fba26bf84d180dc1a4ef015
rittycheriah <rittycheriah@gmail.com>
2015-05-15 20:20:39 -0500

    Login post working

--- jadeapp/routes/users.js
+++ jadeapp/routes/users.js
@@ -1,4 +1,3 @@
-
 var Q = require("q");
 var express = require('express');
 var app = express.Router();
@@ -7,7 +6,7 @@ var UserModel = require('../models/user');
 var auctionItems = require('../models/auctionItem');
 
 //Sending an error
-var sendError = function (req, res, err, message) {
+var sendError = function(req, res, err, message) {
   console.log('Render the error template back to the client.');
   res.render("error", {
     error: {
@@ -19,38 +18,50 @@ var sendError = function (req, res, err, message) {
 };
 
 //Handle the get request for registration form 
-app.get("/register", function (req, res) {
-	console.log('hit register');
+app.get("/register", function(req, res) {
+  console.log('hit register');
   res.render("register");
 });
 
 //Handle the post request from the registration form 
 app.post('/register', function(req, res) {
-	var newUser = new UserModel(req.body);
-	console.log('here is the new user', newUser)
+  var newUser = new UserModel(req.body);
+  console.log('here is the new user', newUser)
 
-	newUser.save(function(err, user) {
-		console.log('going to the redirect');
-		if(err) {
-			sendError(req, res, err, 'Failed to register user');
-		} else {
-			res.redirect('/');
-		}
-	});
+  newUser.save(function(err, user) {
+    console.log('going to the redirect');
+    if (err) {
+      sendError(req, res, err, 'Failed to register user');
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 //Get to Login
-app.get("/login", function (req, res) {
-	console.log('hit login');
+app.get("/login", function(req, res) {
+  console.log('hit login');
   res.render("login");
 });
 
 //Handling a login action 
-app.post('/login', function(){
-	console.log('Node handling login route');
+app.post('/login', function(req, res) {
+  console.log('Node handling login route');
 
-	//Try to log user w/ given info
-	UserController.login(req.body.username, req.body.password)
+  //Try to log user w/ given info
+  UserController.login(req.body.username, req.body.password)
+    //After the database call is complete and successful, promise returns user obj.
+    .then(function (validUser) {
+      console.log('back in route handling user obj.');
+      console.log('validUser =', validUser);
+      res.render('auction');
+    })
+
+  //Database call made, but failed
+  // .fail(function (err) {
+  //   console.log('Failed to look up user');
+  //   sendError(req, res, {errors: err.message}, 'Failed Hard')
+  // })
 });
 
 module.exports = app;
