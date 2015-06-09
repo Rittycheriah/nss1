6b844fcf53a94895f0fcc71dae7e010099c9397e
rittycheriah <rittycheriah@gmail.com>
2015-06-08 22:58:27 -0500

    added gradients

--- colorApp/js/main.js
+++ colorApp/js/main.js
@@ -1,6 +1,8 @@
 var myApp = angular.module('myModule', []);
 
-var colorArray = ['blue', 'green', 'black', 'pink', 'red', 'yellow'];
+var colorArray = ['blue', 'green', 'thistle', 'pink', 'red', 'yellow', 'lightcoral',
+ 'aqua', 'chartreuse','darkcyan', 'darkolivegreen','darksalmon','indianred','lightblue',
+ 'turquoise', 'lightseagreen','lightskyblue','cornflowerblue'];
 
 myApp.controller('colorController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
