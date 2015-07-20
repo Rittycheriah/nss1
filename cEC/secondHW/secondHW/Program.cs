ad42fdae7f2c88750e0cd6d8415145a54e2baeee
rittycheriah <rittycheriah@gmail.com>
2015-07-20 17:26:46 -0500

    second Saturday HW

---
+++ cEC/secondHW/secondHW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+
+namespace secondHW
+{
+	class secondHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine ("What number would you like to choose?");
+			int upperLimit = int.Parse(Console.ReadLine());
+			List<int> evenNums = new List<int>();
+
+			for (int i = 1; i < upperLimit; i++) 
+			{
+				if (i % 2 == 0) 
+				{
+					evenNums.Add(i);
+					Console.WriteLine (i);
+				}	
+			}
+
+			int totalSum = evenNums.Sum ();
+			Console.WriteLine ("The total of the even numbers for your choice {0}", totalSum);
+
+		}
+	}
+}
