b3a1e1be0a8dcf1e2e63df311a904e63a89ad00b
rittycheriah <rittycheriah@gmail.com>
2015-07-21 21:08:43 -0500

    comparison of number exercise completed and working

---
+++ cEC/personalEC/caveEx/caveEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace caveEx
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Please enter a number");
+			string userInput = Console.ReadLine ();
+
+			int resultCon;
+			bool result = Int32.TryParse(userInput, out resultCon);
+
+			if (result) {
+				if (resultCon > 10) {
+					Console.WriteLine ("The number is too small");
+				} else {
+					Console.WriteLine ("The number is just right");				
+				}
+			} else {
+				Console.WriteLine ("TryParse didn't work");
+			}
+	
+		}
+	}
+}
